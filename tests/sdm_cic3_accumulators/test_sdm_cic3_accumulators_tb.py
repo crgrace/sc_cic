e9f895e17613d6d38bf43880bf432aea96d7c96d
Qiang Du <QDu@lbl.gov>
2025-02-21 18:38:51 -0800

    Add test cases of sdm_cic3 and sdm_cic3_accumulators in tests/

---
+++ tests/sdm_cic3_accumulators/test_sdm_cic3_accumulators_tb.py
@@ -0,0 +1,18 @@
+import logging
+import cocotb
+from cocotb.clock import Clock
+from cocotb.triggers import RisingEdge
+
+
+class TB:
+    def __init__(self, dut):
+        dut._log.setLevel(logging.INFO)
+        self.dut = dut
+        cocotb.start_soon(Clock(dut.clk, 5, units="ns").start())
+
+
+@cocotb.test(timeout_time=100, timeout_unit='us')
+async def test_sdm_cic3_accumulators_tb(dut):
+    tb = TB(dut)
+
+    await RisingEdge(tb.dut.clk)
