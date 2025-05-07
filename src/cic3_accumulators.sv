eac3d39aa4dab2a8350a547e001b42e988291fa8
George M <mihelog@lbl.gov>
2025-05-07 14:40:23 -0700

    Seems to work with serial adders.

--- src/cic3_accumulators.sv
+++ src/cic3_accumulators.sv
@@ -50,26 +50,112 @@ always_comb begin : clock_assign
     divided_clk = clock_counter[CLOCK_WIDTH-1]; 
 end // always_comb
 
+
+wire [NUMBITS-1:0] adder1_output;
+wire adder1_done, adder1_cout;
+serial_adder_top #(.WORDWIDTH(NUMBITS)) adder1(
+        .out(adder1_output),
+        .done(adder1_done),
+        .cout(adder1_cout),
+        .a_in(acc1),
+        .b_in(in_coded),
+        .clk(clk),
+        .rst_n(reset_n)
+);
+
+
+wire [NUMBITS-1:0] adder2_output;
+wire adder2_done, adder2_cout;
+serial_adder_top #(.WORDWIDTH(NUMBITS)) adder2(
+	.out(adder2_output),
+	.done(adder2_done),
+	.cout(adder2_cout),
+	.a_in(acc1),
+	.b_in(acc2),
+	.clk(clk),
+	.rst_n(reset_n)
+);
+
+
+wire [NUMBITS-1:0] adder3_output;
+wire adder3_done, adder3_cout;
+serial_adder_top #(.WORDWIDTH(NUMBITS)) adder3(
+        .out(adder3_output),
+        .done(adder3_done),
+        .cout(adder3_cout),
+        .a_in(acc2),
+        .b_in(acc3),
+        .clk(clk),
+        .rst_n(reset_n)
+);
+
 // integrators
-always_ff @ (posedge clk or negedge reset_n) begin 
-    if (!reset_n) begin 
-        acc1 <= 'b0; 
-        acc2 <= 'b0; 
-        acc3 <= 'b0; 
-    end 
+always_ff @ (posedge clk or negedge reset_n) begin
+    if (!reset_n) begin
+        acc1 <= 'b0;
+    end
+    else if (adder1_done) begin
+        acc1 <= adder1_output;
+    end
     else begin
-        acc1 <= acc1 + in_coded; 
-        acc2 <= acc2 + acc1; 
-        acc3 <= acc3 + acc2;  
+        acc1 <= acc1;
     end
 end // always_ff
 
+// integrators
+always_ff @ (posedge clk or negedge reset_n) begin
+    if (!reset_n) begin
+        acc2 <= 'b0;
+    end
+    else if (adder2_done) begin
+        acc2 <= adder2_output;
+    end
+    else begin
+        acc2 <= acc2;
+    end
+end // always_ff
+
+// integrators
+always_ff @ (posedge clk or negedge reset_n) begin
+    if (!reset_n) begin
+        acc3 <= 'b0;
+    end
+    else if (adder3_done) begin
+        acc3 <= adder3_output;
+    end
+    else begin
+        acc3 <= acc3;
+    end
+end // always_ff
+
+// integrators
+//always_ff @ (posedge clk or negedge reset_n) begin 
+//    if (!reset_n) begin 
+//        acc1 <= 'b0; 
+//        acc2 <= 'b0; 
+//        acc3 <= 'b0; 
+//    end 
+//    else begin
+//        acc1 <= acc1 + in_coded; 
+//        acc2 <= acc2 + acc1; 
+//        acc3 <= acc3 + acc2;  
+//    end
+//end // always_ff
+
 // clock divider
+
+// The below assumes that all three serial adders are done at the same time
+// which should be the case since they are configured for the same width and
+// their inputs are driven in at the same time.
 always_ff @ (posedge clk or negedge reset_n) begin
     if (!reset_n)
         clock_counter <= 'b0; 
-    else 
+    else if (adder1_done) begin 
         clock_counter <= clock_counter + 1'b1;
+    end
+    else begin
+	clock_counter <= clock_counter;
+    end
 end // always_ff
 
 // downsampler 
